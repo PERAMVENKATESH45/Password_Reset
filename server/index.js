f9fe79463df954cd34c14eb6a1507533b9226b02
Peram venkatesh <142659219+PERAMVENKATESH45@users.noreply.github.com>
2025-06-22 14:14:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDb from './Database/dbConfig.js';
-import authRouter from './Routers/authRouter.js'
-
+import authRouter from './Routers/authRouter.js';
 
 dotenv.config();
 
@@ -12,16 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-connectDb()
+connectDb();
 
-app.get('/', (req, res)=>{
-    res.status(200).send("Welcome to my API")
+app.get('/', (req, res) => {
+  res.status(200).send("Welcome to my API");
 });
 
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log("Server started and running on the port")
-})
+  console.log(`Server started and running on port ${port}`);
+});

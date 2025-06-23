34f0bd6fc9d3b382f1b051274b9ad641cc833960
Peram venkatesh <142659219+PERAMVENKATESH45@users.noreply.github.com>
2025-06-23 12:05:08 +0530

    Update dbConfig.js

--- server/Database/dbConfig.js
+++ server/Database/dbConfig.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Database connected successfully")
     } catch (error) {
         console.log(error)
